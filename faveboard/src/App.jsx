c9fce70d1461c5f7456da53176b1472a5d9bdd85
ctran42 <cxt4513@mavs.uta.edu>
2025-08-01 15:41:29 -0500

    Set up tailwindcss

--- faveboard/src/App.jsx
+++ faveboard/src/App.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom"
 import './App.css'
 
 function App() {
   return (
     <>
-      <h2>FaveBoard</h2>
+      <h2 className="text-3xl font-bold underline">FaveBoard</h2>
       <Outlet />
     </>
   )
