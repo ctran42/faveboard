156b251031396a839c031ace721754fd31fb24b0
ctran42 <cxt4513@mavs.uta.edu>
2025-08-02 14:50:10 -0500

    Stylized home page

--- faveboard/src/components/BoardGrid.jsx
+++ faveboard/src/components/BoardGrid.jsx
@@ -7,11 +7,13 @@ import testboard2 from '../assets/testboard2.png'
 const BoardGrid = () => {
     const [boards, setBoards] = useState([
         { id: 1, title: "My Board #1", imageLink: testboard1, createdAt: "", lastEdited: "" },
-        { id: 2, title: "My Favorite", imageLink: testboard2, createdAt: "", lastEdited: "" }
+        { id: 2, title: "My Favorite", imageLink: testboard2, createdAt: "", lastEdited: "" },
+        { id: 3, title: "My Board #3", imageLink: testboard2, createdAt: "", lastEdited: "" },
+        { id: 4, title: "My Board #4", imageLink: testboard2, createdAt: "", lastEdited: "" }
     ])
 
     return (
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-4 flex mx-auto mt-10 max-w-xl w-full">
             {boards.map((board) => (
                 <BoardPreview 
                     key={board.id} 
--- faveboard/src/components/NewBoardCard.jsx
+++ faveboard/src/components/NewBoardCard.jsx
@@ -4,13 +4,15 @@ const NewBoardCard = () => {
     }
 
     return (
-        <button
-            onClick={handleNewBoard}
-            className="flex flex-col items-center justify-center w-24 h-24 bg-black text-white border border-gray-600 rounded-md hover:bg-gray-800 transition"
-        >
-            <span className="text-4xl">+</span>
+        <div className="flex flex-col items-center">
+            <button
+                onClick={handleNewBoard}
+                className="justify-center w-24 h-24 bg-black text-white border border-gray-600 rounded-md hover:bg-gray-800 transition"
+            >
+                <span className="text-4xl">+</span>
+            </button>
             <span className="mt-1 text-sm text-gray-300">New board</span>
-        </button>
+        </div>
     )
 }
 
